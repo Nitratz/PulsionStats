e4fce28c14987a630c5aa67a76ce117536262021
Maxime liege <liege30@gmail.com>
2018-03-06 13:02:25 +0100

    Heroku fix bug on port

--- bot.js
+++ bot.js
@@ -2,6 +2,16 @@ let Discord = require('discord.io');
 let logger = require('winston');
 let auth = require('./auth.json');
 let request = require('request');
+var express = require('express');
+var app     = express();
+
+app.set('port', (process.env.PORT || 5000));
+app.get('/', function(request, response) {
+    var result = 'App is running'
+    response.send(result);
+}).listen(app.get('port'), function() {
+    console.log('App is running, server is listening on port ', app.get('port'));
+});
 
 logger.remove(logger.transports.Console);
 logger.add(logger.transports.Console, {
